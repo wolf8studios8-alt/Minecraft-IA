d3232bd800777dbdd2eb9c54dc56ab5fd69c4d89
wolf8studios8-alt <wolf8studios8@gmail.com>
2026-03-10 22:48:34 +0100

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+// Configuraciones globales del mundo
+export const CHUNK_WIDTH = 16;
+export const CHUNK_HEIGHT = 128;
+export const CHUNK_DEPTH = 16;
+export const WATER_LEVEL = 30;
+export const RENDER_DISTANCE = 4;
+export const UNLOAD_DISTANCE = 5;
+
+// Diccionario de IDs de los bloques para todo el juego
+export const BLOCKS = { 
+    AIR: 0, 
+    DIRT: 1, 
+    GRASS: 2, 
+    STONE: 3, 
+    WOOD: 4, 
+    LEAVES: 5, 
+    SAND: 6, 
+    WATER: 7, 
+    TORCH: 8, 
+    CHEST: 9, 
+    GLASS: 10 
+};
